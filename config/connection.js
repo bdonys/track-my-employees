cf15684542574c3ca915978031987ad6e7bff94b
bdonys <brandonsur@yahoo.com>
2024-01-09 12:09:18 -0800

    connections file

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+}).promise();
+
+
+
+module.exports = connection;
